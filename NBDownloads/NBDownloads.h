f789c2d64d0cc0732552fd8a59f74dd9bedc9c7b
Nithin Bhaktha <nbhaktha@vmware.com>
2022-10-17 14:42:40 +0530

    Initial Commit

---
+++ NBDownloads/NBDownloads.h
@@ -0,0 +1,18 @@
+//
+//  NBDownloads.h
+//  NBDownloads
+//
+//  Created by Nithin Bhaktha on 17/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NBDownloads.
+FOUNDATION_EXPORT double NBDownloadsVersionNumber;
+
+//! Project version string for NBDownloads.
+FOUNDATION_EXPORT const unsigned char NBDownloadsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NBDownloads/PublicHeader.h>
+
+
